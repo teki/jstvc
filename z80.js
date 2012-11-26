0139a0339fd244e1762b765b530ee96cf02f47a9
teki <teki321@gmail.com>
2012-11-26 07:48:22 -0500

    rewrite js objects

--- z80.js
+++ z80.js
@@ -15,7 +15,7 @@ function toHex16(x) {
 	return "000".slice(s.length - 1) + s;
 }
 
-function decodeZ80(addr) {
+function decodeZ80(mmu, addr) {
 	var tableR = ["B", "C", "D", "E", "H", "L", "(HL)", "A"],
 		tableRP = ["BC", "DE", "HL", "SP"],
 		tableRP2 = ["BC", "DE", "HL", "AF"],
@@ -40,40 +40,40 @@ function decodeZ80(addr) {
 		ox, oy, oz, op, oq;
 
 	function fetchdByte() {
-		od = MMU.r8(addr+idx);
+		od = mmu.r8(addr+idx);
 		idx++;
 	}
 
 	function fetchnByte() {
-		on = MMU.r8(addr+idx);
+		on = mmu.r8(addr+idx);
 		idx++;
 	}
 
 	function fetchnWord() {
-		onn = MMU.r8(addr+idx);
+		onn = mmu.r8(addr+idx);
 		idx++;
-		onn += MMU.r8(addr+idx) << 8;
+		onn += mmu.r8(addr+idx) << 8;
 		idx++;
 	}
 
 	// fetch opcode
-	opcode = MMU.r8(addr+idx);
+	opcode = mmu.r8(addr+idx);
 	idx++;
 	prefix1 = 0;
 	prefix2 = 0;
 	if ((opcode == 0xCB) || (opcode == 0xED)) {
 		prefix1 = opcode;
-		opcode = MMU.r8(addr+idx);
+		opcode = mmu.r8(addr+idx);
 		idx++;
 	}
 	else if ((opcode == 0xDD) || (opcode == 0xFD)) {
 		throw "DD FD not implemented";
 		prefix1 = opcode;
-		opcode = MMU.r8(addr+idx);
+		opcode = mmu.r8(addr+idx);
 		idx++;
 		if (opcode == 0xCB) {
 			prefix2 = opcode;
-			opcode = MMU.r8(addr+idx);
+			opcode = mmu.r8(addr+idx);
 			idx++;
 		}
 	}
@@ -236,238 +236,244 @@ function decodeZ80(addr) {
 	return ["- " + toHex8(opcode), idx];
 }
 
-var MMU = {
-    _u0: new Uint8Array(16384),
-    _u1: new Uint8Array(16384),
-    _u2: new Uint8Array(16384),
-    _u3: new Uint8Array(16384),
-//    _cart: Uint8Array(new ArrayBuffer(16384)),
-    _sys: [],
-    _ext: new n_buffer.Buffer(16384),
-    _vid: new Uint8Array(16384),
-    _map: [],
-    _mapVal: -1,
+/* *********************************************
+    MMU
+********************************************* */
+function MMU() {
+    this._u0 = new Uint8Array(16384);
+    this._u1 = new Uint8Array(16384);
+    this._u2 = new Uint8Array(16384);
+    this._u3 = new Uint8Array(16384);
+//    this._cart = Uint8Array(new ArrayBuffer(16384));
+    this._sys = [];
+    this._ext = new n_buffer.Buffer(16384);
+    this._vid = new Uint8Array(16384);
+    this._map = [];
+    this._mapVal = -1;
     
-    init: function() {
-        var i;
-        for(i=0; i<MMU._u0.length; i++) MMU._u0[i] = 0;
-        for(i=0; i<MMU._u1.length; i++) MMU._u1[i] = 0;
-        for(i=0; i<MMU._u2.length; i++) MMU._u2[i] = 0;
-        for(i=0; i<MMU._u3.length; i++) MMU._u3[i] = 0;
-//        for(i=0; i<MMU._cart.length; i++) MMU._cart[i] = 0;
-        for(i=0; i<MMU._ext.length; i++) MMU._ext[i] = 0;
-        
-        var ext = n_fs.readFileSync("TVC_EXT.ROM");
-        ext.copy(MMU._ext);
-        
-        MMU._sys = n_fs.readFileSync("TVC_SYS.ROM");
-        MMU.setMap(0);
-    },
-    
-    reset: function() {
-        MMU.setMap(0);
-    },
-    
-    setMap: function(val) {
-        if (val == MMU._mapVal) return;
+    this.init();
+};
 
-        // page 3
-        var page3 = (val & 0xc0) >> 6;
-        if (page3 == 0) MMU._map[3] = MMU._sys;// MMU._cart;
-        else if (page3 == 1) MMU._map[3] = MMU._sys;
-        else if (page3 == 2) MMU._map[3] = MMU._u3;
-        else MMU._map[3] = MMU._ext;
-        // page 2
-        if (val & 0x20) MMU._map[2] = MMU._u2;
-        else MMU._map[2] = MMU._vid;
-        // page 1 is always u1
-        // page 0
-        var page0 = (val & 0x18) >> 3;
-        if (page0 == 0) MMU._map[0] = MMU._sys;
-        else if (page0 == 1) MMU._map[0] = MMU._sys;// MMU._cart;
-        else if (page3 == 2) MMU._map[0] = MMU._u0;
-        else throw "do not know";
-    },
-    getMap: function() {
-        return MMU._mapVal;
-    },
+MMU.prototype.init = function() {
+    var i;
+    for(i=0; i< this._u0.length; i++) this._u0[i] = 0;
+    for(i=0; i< this._u1.length; i++) this._u1[i] = 0;
+    for(i=0; i< this._u2.length; i++) this._u2[i] = 0;
+    for(i=0; i< this._u3.length; i++) this._u3[i] = 0;
+//        for(i=0; i<this._cart.length; i++) this._cart[i] = 0;
+    for(i=0; i<this._ext.length; i++) this._ext[i] = 0;
     
-    w8: function(addr, val) {
-        addr = addr & 0xFFFF;
-        val = val & 0xFF;
-        var mapIdx = addr >>> 14;
-        var block = MMU._map[mapIdx];
-        if (block === MMU._sys || block === MMU._ext)
-            return;
-        block[addr & 0x3FFF] = val;
-    },
-    w16: function(addr, val) {
-        MMU.w8(addr+1, val >>> 8);
-        MMU.w8(addr, val & 0xFF);
-    },
-    r8: function(addr) {
-        addr = addr & 0xFFFF;    
-        var mapIdx = addr >>> 14;
-        return MMU._map[mapIdx][addr & 0x3FFF];
-    },
-    r16: function(addr) {
-        return (MMU.r8(addr+1) << 8) | MMU.r8(addr);
-    },
+    var ext = n_fs.readFileSync("TVC_EXT.ROM");
+    ext.copy(this._ext);
     
-    dasm: function(addr, lines) {
-        var offset = 0,
-            d, i, str, oplen;
-        do {
-            d = decodeZ80(addr + offset);
-            oplen = d[1];
-            
-            str = toHex16(addr + offset) + " ";
-            for(i = 0; i < 4; i++) {
-                if (i < oplen) {
-                    str += toHex8(MMU.r8(addr + offset + i)) + " ";
+    this._sys = n_fs.readFileSync("TVC_SYS.ROM");
+    this.setMap(0);
+};  
+MMU.prototype.reset = function() {
+    this.setMap(0);
+};
+MMU.prototype.setMap = function(val) {
+    if (val == this._mapVal) return;
+    
+    // page 3
+    var page3 = (val & 0xc0) >> 6;
+    if (page3 === 0) this._map[3] = this._sys;// this._cart;
+    else if (page3 == 1) this._map[3] = this._sys;
+    else if (page3 == 2) this._map[3] = this._u3;
+    else this._map[3] = this._ext;
+    // page 2
+    if (val & 0x20) this._map[2] = this._u2;
+    else this._map[2] = this._vid;
+    // page 1 is always u1
+    // page 0
+    var page0 = (val & 0x18) >> 3;
+    if (page0 === 0) this._map[0] = this._sys;
+    else if (page0 == 1) this._map[0] = this._sys;// this._cart;
+    else if (page3 == 2) this._map[0] = this._u0;
+    else throw "do not know";
+};
+MMU.prototype.getMap = function() {
+    return this._mapVal;
+};
+MMU.prototype.w8 = function(addr, val) {
+    addr = addr & 0xFFFF;
+    val = val & 0xFF;
+    var mapIdx = addr >>> 14;
+    var block = this._map[mapIdx];
+    if (block === this._sys || block === this._ext)
+        return;
+    block[addr & 0x3FFF] = val;
+};
+MMU.prototype.w16 = function(addr, val) {
+    this.w8(addr+1, val >>> 8);
+    this.w8(addr, val & 0xFF);
+};
+MMU.prototype.r8 = function(addr) {
+    addr = addr & 0xFFFF;    
+    var mapIdx = addr >>> 14;
+    return this._map[mapIdx][addr & 0x3FFF];
+};
+MMU.prototype.r16 = function(addr) {
+    return (this.r8(addr+1) << 8) | this.r8(addr);
+};
+MMU.prototype.dasm = function(addr, lines) {
+    var offset = 0,
+        d, i, str, oplen;
+    do {
+        d = decodeZ80(this, addr + offset);
+        oplen = d[1];
+        
+        str = toHex16(addr + offset) + " ";
+        for(i = 0; i < 4; i++) {
+            if (i < oplen) {
+                str += toHex8(this.r8(addr + offset + i)) + " ";
+            }
+            else {
+                str += "   ";
+            }
+        }
+        console.log(str + d[0] + "\n");
+        offset += oplen;
+        lines--;
+    } while (lines);
+};
+
+/* *********************************************
+    Z80State
+********************************************* */
+function Z80State() {
+    this.init();
+}
+
+Z80State.prototype.getBC = function() { return (this.B << 8) | this.C;};
+Z80State.prototype.setBC = function(val) { this.B = (val >>> 8);  this.C = val & 0xFF;};
+Z80State.prototype.getDE = function() { return (this.D << 8) | this.E;};
+Z80State.prototype.setDE = function(val) { this.D = (val >>> 8);  this.E = val & 0xFF;};
+Z80State.prototype.getHL = function() { return (this.H << 8) | this.L;};
+Z80State.prototype.setHL = function(val) { this.H = (val >>> 8);  this.L = val & 0xFF;};
+
+Z80State.prototype.init = function() {
+    // interrupt
+    this.im = 0;
+    this.IFF1 = 0;
+    this.IFF2 = 0;
+    this.I = 0xFF;
+    
+    // registers
+    this.A = 0xFF;
+    this.B = 0xFF;
+    this.C = 0xFF;
+    this.D = 0xFF;
+    this.E = 0xFF;
+    this.F = 0xFF;
+    this.H = 0xFF;
+    this.L = 0xFF,
+    this.SP = 0xFFFF;
+    this.PC = 0x0000;
+    
+    this.IX = 0xFFFF;
+    this.IY = 0xFFFF;
+    
+    this.AFa = 0xFFFF;
+    this.BCa = 0xFFFF,
+    this.DEa = 0xFFFF;
+    this.HLa = 0xFFFF;
+    
+    this.R = 0xFF;
+
+    this.reset();
+};
+
+Z80State.prototype.reset = function() {
+    this.IFF1 = 0;
+    this.IFF2 = 0;
+    this.PC = 0;
+    this.R = 0;
+    this.I = 0;
+    this.im = 0;
+};
+
+/* *********************************************
+    Z80
+********************************************* */
+function Z80(mmu) {
+    this._mmu = mmu;
+    this._s = new Z80State();
+    this._opcodes = {
+        0x00: {
+            t: 4, m: 1, asm: "NOP",
+            f: function(reg, mem) {}
+            },
+        0x3e: {
+            t: 7, m: 2, asm: "LD A,n",
+            f: function(reg, mem) {
+                reg.A = mem.r8(reg.PC + 1);
+                throw("set flags!!!!");
                 }
-                else {
-                    str += "   ";
+            },
+        0xc3: {
+            t: 10, m: 0, asm: "JP (nn)", // m hack, it is 3, but don't want to skip
+            f: function(reg, mem) {
+                reg.PC = mem.r16(reg.PC + 1);
                 }
-            }
-            console.log(str + d[0] + "\n");
-            offset += oplen;
-            lines--;
-        } while (lines);
-    }
+            },
+        0xf3: {
+            t: 4, m: 1, asm: "DI",
+            f: function(reg, mem) {
+                reg.IFF1 = 0;
+                reg.IFF2 = 0;
+                }
+            },
+        0xed56: {
+            t: 8, m: 2, asm: "IM 1",        // RST 38
+            f: function(reg, mem) {
+                reg.im = 1;
+                }
+            },
+    };        
 };
 
-var TVC = {
-    _clock: 0,
-    run: function() {
-        var limit = 100;
-        MMU.init();
-        Z80.init();
-        while (TVC._clock < limit) {
-            var tinc = Z80.step();
-            TVC.clock += tinc;
+Z80.prototype.step = function() {
+    var opcode = this._mmu.r8(this._s.PC);
+    if (opcode == 0xed || opcode == 0xcb || opcode == 0xdd || opcode == 0xfd) {
+        opcode = (opcode << 8) | this._mmu.r8(this._s.PC + 1);
+        if (opcode == 0xfdcb || opcode == 0xddcb) {
+            opcode = (opcode << 8) | this._mmu.r8(this._s.PC + 2);
         }
     }
+    var op = this._opcodes[opcode];
+    if (!op) {
+        this._mmu.dasm(this._s.PC, 5);
+        throw("not implemented:" + toHex8(opcode));
+    }
+    op.f(this._s, this._mmu);
+    this._s.PC += op.m;
+    return op.t;
 };
 
-var Z80 = {
-    _im: 0,
-    _r: {
-        A: 0xFF,
-        B: 0xFF,
-        C: 0xFF,
-        D: 0xFF,
-        E: 0xFF,
-        F: 0xFF,
-        H: 0xFF,
-        L: 0xFF,
-        SP: 0xFFFF,
-        PC: 0x0000,
-        
-        IX: 0xFFFF,
-        IY: 0xFFFF,
-        I: 0xFF,
-        R: 0xFF,
-        IFF1: 0,
-        IFF2: 0,
-        
-        AFa: 0xFFFF,
-        BCa: 0xFFFF,
-        DEa: 0xFFFF,
-        HLa: 0xFFFF,
-
-        getBC: function() { return (Z80._r.B << 8) | Z80._r.C;},
-        setBC: function(val) { Z80._r.B = (val >>> 8);  Z80._r.C = val & 0xFF;},
-        getDE: function() { return (Z80._r.D << 8) | Z80._r.E;},
-        setDE: function(val) { Z80._r.D = (val >>> 8);  Z80._r.E = val & 0xFF;},
-        getHL: function() { return (Z80._r.H << 8) | Z80._r.L;},
-        setHL: function(val) { Z80._r.H = (val >>> 8);  Z80._r.L = val & 0xFF;},
-
-
-    },
-    init: function() {
-        Z80._r.A = 0xFF;
-        Z80._r.B = 0xFF;
-        Z80._r.C = 0xFF;
-        Z80._r.D = 0xFF;
-        Z80._r.E = 0xFF;
-        Z80._r.F = 0xFF;
-        Z80._r.H = 0xFF;
-        Z80._r.L = 0xFF;
-        Z80._r.SP = 0xFFFF;
-        Z80._r.PC = 0x0000;
-        
-        Z80._r.IX = 0xFFFF;
-        Z80._r.IY = 0xFFFF;
-        Z80._r.I = 0xFF;
-        Z80._r.R = 0xFF;
-        
-        Z80._r.AFa = 0xFFFF;
-        Z80._r.BCa = 0xFFFF;
-        Z80._r.DEa = 0xFFFF;
-        Z80._r.HLa = 0xFFFF;
-
-        Z80.reset();
-    },
-
-    reset: function() {
-        Z80._r.IFF1 = 0;
-        Z80._r.IFF2 = 0;
-        Z80._r.PC = 0;
-        Z80._r.R = 0;
-        Z80._r.I = 0;
-        Z80._im = 0;
-    },
-    
-    step: function() {
-        var opcode = MMU.r8(Z80._r.PC);
-        if (opcode == 0xed || opcode == 0xcb || opcode == 0xdd || opcode == 0xfd) {
-            opcode = (opcode << 8) | MMU.r8(Z80._r.PC + 1);
-            if (opcode == 0xfdcb || opcode == 0xddcb) {
-                opcode = (opcode << 8) | MMU.r8(Z80._r.PC + 2);
-            }
-        }
-        var op = Z80[opcode];
-        if (!op) {
-            MMU.dasm(Z80._r.PC, 5);
-            throw("not implemented:" + toHex8(opcode));
-        }
-        op.f();
-        Z80._r.PC += op.m;
-        return op.t;
-    },
-        
-    0x00: {
-        t: 4, m: 1, asm: "NOP",
-        f: function() {
-            }
-        },
-    0x3e: {
-        t: 7, m: 2, asm: "LD A,n",
-        f: function() {
-            Z80._r.A = MMU.r8(Z80._r.PC + 1);
-            throw("set flags!!!!");
-            }
-        },
-    0xc3: {
-        t: 10, m: 0, asm: "JP (nn)", // m hack, it is 3, but don't want to skip
-        f: function() {
-            Z80._r.PC = MMU.r16(Z80._r.PC + 1);
-            }
-        },
-    0xf3: {
-        t: 4, m: 1, asm: "DI",
-        f: function() {
-            Z80._r.IFF1 = 0;
-            Z80._r.IFF2 = 0;
-            }
-        },
-    0xed56: {
-        t: 8, m: 2, asm: "IM 1",        // RST 38
-        f: function() {
-            Z80._im = 1;
-            }
-        },
+Z80.prototype.reset = function() {
+    this._s.reset();
 };
 
-TVC.run();
+/* *********************************************
+    TVC
+********************************************* */
+function TVC() {
+    this._clock = 0;
+    this._mmu = new MMU();
+    this._z80 = new Z80(this._mmu);
+}
+TVC.prototype.run = function() {
+    var limit = 100;
+    while (this._clock < limit) {
+        var tinc = this._z80.step();
+        this.clock += tinc;
+    }
+}
+
+/* *********************************************
+    runner
+********************************************* */
+
+var tvc = new TVC();
+tvc.run();
